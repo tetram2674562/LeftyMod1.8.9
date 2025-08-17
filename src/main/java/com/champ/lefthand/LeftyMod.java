8d23dfe4981691e5bc0a750a5432b7be07bc8828
tetram2674562 <dev@tetram26.org>
2025-08-17 18:26:02 +0200

    Changed modloader to legacy fabric

--- src/main/java/com/champ/lefthand/LeftyMod.java
+++ src/main/java/com/champ/lefthand/LeftyMod.java
@@ -4,21 +4,16 @@ import com.champ.lefthand.mixins.MixinGameSettings;
 import com.champ.lefthand.usage.ToggleScreen;
 import net.minecraft.client.Minecraft;
 import net.minecraft.init.Blocks;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.fabricmc.api.ModInitializer;
 
-@Mod(modid = LeftyMod.MODID, version = LeftyMod.VERSION, name = LeftyMod.NAME)
-public class LeftyMod {
-    public static final String MODID = "lefthand";
-    public static final String VERSION = "1.0";
-    public static final String NAME = "Lefty Mod";
+public class LeftyMod implements ModInitializer{
 
     public static LeftyMod INSTANCE = new LeftyMod();
 
     private static Minecraft mc = Minecraft.getMinecraft();
 
-    @Mod.EventHandler
-    public void init(FMLInitializationEvent event) {
+    @Override
+    public void onInitialize(){
         System.out.println("STARTING MOD-------------------------------------------");
     }
 }
